2ca0b9f592ab74fbc1a5bfe91ccc0f06c92b7d38
Mohamed Haki <Mohamedhaki70@gmail.com>
2024-10-07 15:24:47 +0100

    Add utility components: LoadingIndicator, Divider

---
+++ src/Components/Divider.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Divider = () => (
+  <div className="my-8 flex items-center before:flex-1 before:border-t before:border-neutral-300 dark:before:border-neutral-700 after:flex-1 after:border-t after:border-neutral-300 dark:after:border-neutral-700">
+    <p className="mx-4 text-center text-gray-800 dark:text-gray-200">Or</p>
+  </div>
+);
+
+export default Divider;
